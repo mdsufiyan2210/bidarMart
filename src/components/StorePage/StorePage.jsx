b338e110ddde23551add505ac95d3e1056fe2170
iamtariq555 <iamtariq555@gmail.com>
2023-03-01 15:18:37 +0530

    hi

--- src/components/StorePage/StorePage.jsx
+++ src/components/StorePage/StorePage.jsx
@@ -8,6 +8,7 @@ import allActions from "../../actions";
 
 export default function StorePage() {
   const [data, setData] = useState([])
+  const [copydata,setcopydata]=useState(1)
   const apiCall = async (params) => {
     const response = await axios.get('https://dummyjson.com/products')
     setData(response.data.products)
@@ -18,15 +19,22 @@ export default function StorePage() {
   }, [])
 
 const dispatch = useDispatch()
-const add = (params) => {
+const add = (add) => {
   dispatch(allActions.cardAction())
+  dispatch(allActions.itemAction(copydata))
+  dispatch(allActions.price(add))  
 }
 
+useEffect(()=>{
+  if(copydata <=0){
+    setcopydata(1)
+  }
+},[copydata])
 
   return (
     <>
       <div className="container">
-        {data.map((data) => {
+        {data.map((data,key) => {
           return (
             <div className="items">
               <SimpleImageSlider
@@ -41,7 +49,9 @@ const add = (params) => {
               <p>{data.description}</p>
               <p>{data.price}</p>
               <p>{data.discountPercentage}</p>
-              <button onClick={add}>Add to cart</button>
+              <button onClick={()=>add(data.price)}>Add to cart</button>
+              <label htmlFor={`faraz${key}`}  style={{width:"30px",marginLeft:"50px"}}>no of items:</label>
+              <input type="number" id={`faraz${key}`} style={{width:"30px",marginLeft:"5px"}} value={copydata} onChange={(e)=>{setcopydata(e.target.value)}}/>
             </div>
           )
         })}
